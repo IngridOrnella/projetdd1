efd85eb711fe488993d2ee06c7e0e8135f58e391
Ingrid Fondja <Ingridornella14@yahoo.com>
2020-12-04 07:10:27 +0100

    mon premier commit

---
+++ src/spjrud/ex.py
@@ -0,0 +1,12 @@
+'''
+Created on 3 déc. 2020
+
+@author: Ingrid Fondja
+'''
+import numpy as np
+import scipy.stats as sp_st
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy
+from collections import defaultdict as dfd
+import scipy.optimize as spop
