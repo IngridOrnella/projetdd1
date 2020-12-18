8077d72fe71d712fcf5b53f555bf0f557d0dd160
Ingrid Fondja <Ingridornella14@yahoo.com>
2020-12-18 23:45:41 +0100

    rajout des methodes dans les classes djru

--- src/spjrud/Expr.py
+++ src/spjrud/Expr.py
@@ -1,17 +1,23 @@
 import sqlite3
+import Relation
 
-class Expression():
+class Expr(SPRJUD):
 	"""Cette classe represente une expression ecrite en spjrud. elle est la classe mere de notre AST"""
 
 
-	def __init__(self,nom):
-		self.nom = nom
-		self.exp= 
+	def __init__(self,relation):
+		self.relation = relation
+		self.col= relation.col 
 
 
 	def __str__(self):
 		return
+
+	def print_req(self):
+		print(self.requete)
+
+	def string_req(self):
+		return self.requete
 		
 
 
-print("hello world")
